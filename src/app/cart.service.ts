4fc126f51332897cfc5e2d1f1b22f79708bc6681
Sashikumar <yshashi30@gmail.com>
2023-11-19 13:17:38 +0530

    feat: updated the mutate with update

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -11,29 +11,31 @@ export class CartService {
   }, 0));
   public totalItems = computed(() => this.cartItems().length);
 
-  constructor(private api: ApiService){
+  constructor(private api: ApiService) {
 
   }
 
   addProductSignal(product: IProduct) {
-    this.cartItems.mutate((val) => {
-      val.push(product);
+    this.cartItems.update((val) => {
+      return [...val, product]
     });
-    this.api.products()?.forEach(a=>{
-      if(a.id === product.id){
+    this.api.products()?.forEach(a => {
+      if (a.id === product.id) {
         a.rating.count = a.rating.count - 1;
       }
     })
   }
 
   removeProductSignal(id: number) {
-    this.cartItems.mutate(val => {
+    this.cartItems.update(val => {
       const product = val.splice(id, 1);
       this.api.products()?.forEach(a => {
         if (a.id === product[0].id) {
           a.rating.count = a.rating.count + 1;
         }
-      })
-    })
+      });
+      return val;
+    });
+
   }
 }
